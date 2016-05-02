88b92f94ef6a88fd1c2de476c86e615cdd4b1588
John Hilts <johnhilts@live.com>
2016-05-01 18:26:55 -0700

    Video 3

--- app/index.js
+++ app/index.js
@@ -1,16 +1,54 @@
+var USER_DATA = {
+	name: 'John Hilts',
+	username: 'johnhilts',
+	image: 'https://avatars0.githubusercontent.com/u/631407?v=3&s=400'
+}
+
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HellowWorld = React.createClass({
-	render: function() {
+var ProfilePic = React.createClass({
+	render: function () {
+		return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+	}
+});
+
+var ProfileLink = React.createClass({
+	render: function () {
 		return (
-			<div>Hello Worldz!!</div>
+			<div>
+				<a href={'https://www.github.com/' + this.props.username}>
+					{this.props.username}
+				</a>
+			</div>
 		)
 	}
 });
 
+var ProfileName = React.createClass({
+	render: function () {
+		return (
+			<div>{this.props.name}</div>
+		)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
+		)
+	}
+})
+
+
 ReactDOM.render(
-	<HellowWorld />,
+	<Avatar user={USER_DATA} />,
 	document.getElementById('app')
 );
 
